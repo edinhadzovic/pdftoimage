4389e96d60d5564fcd95066a48cf67027ccde275
Edin Hadzovic <eha@icomedias.com>
2020-11-16 14:03:46 +0100

    init commit.

---
+++ index.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const pdftoimage = require('pdftoimage');
+
+const pdfPath = __dirname + '/pdf/form.pdf';
+const outputPath = __dirname + '/out/';
+
+fs.existsSync(pdfPath);
+
+if (!fs.existsSync(pdfPath)) {
+    console.log('pds path not valid');
+    return;
+}
+
+if (!fs.existsSync(outputPath)) {
+    console.log('out path is not valid')
+    return;
+}
+
+const main = function(pdfPath, outputPath) {
+    pdftoimage(pdfPath, {
+        format: 'tiff',
+        prefix: 'tiff',
+        outdir: outputPath,
+    }).then(() => {
+        console.log("Conservation is done");
+    }).catch(error => console.log(error));
+}
+
+
+main(pdfPath, outputPath);
